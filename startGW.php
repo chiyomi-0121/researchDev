c151e1b64d6fd566a4e84cbf8ccd822c0aa0a02f
chiyomi-0121 <norun.dolphin0121@gmail.com>
2022-06-08 14:38:21 +0900

    0608 second commit

---
+++ startGW.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>開始画面</title>
+</head>
+<body>
+    <div id="wrap">
+        <div id="main">
+            <p style="text-align: center; margin-top: 30%;padding: 0;font-size:32px;">開始時間まで<br>お待ちください</p>
+        </div>
+    </div>
+</body>
+</html>
