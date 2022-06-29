f89e5c33bef4486889b15b24fe00739fbc309de7
chiyomi-0121 <norun.dolphin0121@gmail.com>
2022-06-29 16:36:14 +0900

    0629 third commit

--- super/timeSet.php
+++ super/timeSet.php
@@ -1,15 +1,32 @@
+<?php
+    $time = $_POST['timeset'];
+    $date = date("Y/m/d");
+
+    $dateTime = $date . " " . $time;
+
+    file_put_contents("../text/start.txt", $dateTime, FILE_APPEND);
+    //var_dump($dateTime);
+?>
+
 <!DOCTYPE html>
 <html lang="js">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style_super.css">
-    <title>管理者画面</title>
+    <link rel="stylesheet" href="../css/style_super.css">
+    <title>完了画面</title>
 </head>
 <body>
     <div id="wrap">
-        <div id="header"></div>
-        <div id="main"></div>
-        <div id="footer"></div>
+        <div id="header">
+            <h1>登録完了</h1>
+        </div>
+        <div id="main">
+            <h3>下記の時間に開始時刻をセットしました。</h3>
+            <h3 id="printTime"><?= $time ?></h3>
+        </div>
+        <div id="footer">
+            ｵｯｹｰ☆⌒d(´∀｀)ノ
+        </div>
     </div>
 </body>
 </html>
